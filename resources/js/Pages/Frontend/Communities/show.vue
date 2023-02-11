db52013207d9534cdd5e23940d62f27551d86adb
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2023-02-11 13:04:10 +0100

    restructuring of the layout

--- resources/js/Pages/Frontend/Communities/show.vue
+++ resources/js/Pages/Frontend/Communities/show.vue
@@ -1,34 +1,33 @@
 <template>
-<GuestLayout>
-    <template #header>    
-        <div class="flex justify-between m-2 p-2">
-            <h2 class="font-semibold text-xl text-grey-800 leading-tight">
-             {{community.name}}
-            </h2> 
-            <Link  v-if="$page.props.auth.auth_check" as="button" type="button" :href="route('communities.posts.create',community.slug)" class="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-700 px-3 py-2 text-sm
-                font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2
-                focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto mr-3">Create Post
-            </Link> 
-        </div>
-    </template>
-
-    <section class ="flex md:flex-row m-2 p-2">
-        <div class="w-8/12">
-         <PostCard v-for="post in posts.data" :post="post" :community="community.slug" :key="post.id" />
-         <div class="mt-4 p-2">
-         <Pagination :links="posts.meta.links" />
-         </div>
-        </div>
-        <div class="w-4/12">
-            <div class="m-3 p-3 bg-blue-700 text-white mr-6">
-                <h2>Latest Communities</h2>
+    <GuestLayout>
+        <template #header>
+            <div class="flex justify-between m-2 p-2">
+                <h2 class="font-semibold text-xl text-grey-800 leading-tight">
+                {{community.name}}
+                </h2> 
+                <Link  v-if="$page.props.auth.auth_check" as="button" type="button" :href="route('communities.posts.create',community.slug)" class="inline-flex items-center justify-center rounded-md border border-transparent bg-blue-700 px-3 py-2 text-sm
+                    font-medium text-white shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2
+                    focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto mr-3">Create Post
+                </Link> 
             </div>
-        </div>
-    </section>
-    
-</GuestLayout>
-</template>
+        </template>
 
+        <section class="flex flex-col md:flex-row m-2 p-2">
+            <div class="w-full md:w-8/12">
+             <PostCard v-for="post in posts.data" :post="post" :community="community.slug" :key="post.id" />
+              <div class="mt-4 p-2">
+                 <Pagination :links="posts.meta.links" />
+              </div>
+            </div>
+
+            <div class="w-full md:w-4/12 p-4">
+                <div class="m-2 p-2 bg-blue-700 text-white">
+                    <h2>Latest Communities </h2>
+                </div>
+            </div>
+        </section>
+    </GuestLayout>
+</template>
 
 <script setup>
 import GuestLayout from "@/Layouts/GuestLayout.vue"
