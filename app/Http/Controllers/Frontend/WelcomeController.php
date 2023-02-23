93d7dd7cbe51e0f84bd7099e8195a48c2b79cd11
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2023-02-23 08:42:07 +0100

    adding of top communities to the homepage

--- app/Http/Controllers/Frontend/WelcomeController.php
+++ app/Http/Controllers/Frontend/WelcomeController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Models\Post;
 use Inertia\Inertia;
+use App\Models\Community;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CommunityResource;
 use App\Http\Resources\CommunityPostResource;
 
 class WelcomeController extends Controller
@@ -17,6 +19,8 @@ class WelcomeController extends Controller
         $posts =CommunityPostResource::collection(Post::with(['user','community','postVotes'=>function($query){
             $query->where('user_id',auth()->id());
         }])->withCount('comments')->orderBy('votes','desc')->take(12)->get());
-        return Inertia::render('Welcome',compact('posts'));
+
+        $communities =CommunityResource::collection(Community::withCount('posts')->orderBy('posts_count','desc')->take(6)->get());
+        return Inertia::render('Welcome',compact('posts','communities'));
     }
 }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,13 +1,16 @@
 <template>
     <GuestLayout>
         <section class="flex  md:flex-row m-2 p-2">
-            <div class="w-full md:w-8/12">
+            <div class="w-8/12">
              <PostCard v-for="post in posts.data" :post="post" :community="post.community_slug" :key="post.id" />
             </div>
 
-            <div class="w-full md:w-4/12 p-4">
+            <div class="w-4/12 p-4">
                 <div class="m-2 p-2 bg-blue-700 text-white">
                     <h2>Top Communities </h2>
+                </div>
+                <div>
+                 <CommunityList :communities="communities.data" />
                 </div>
             </div>
         </section>
@@ -17,10 +20,11 @@
 <script setup>
 import GuestLayout from "@/Layouts/GuestLayout.vue"
 import PostCard from '@/components/PostCard.vue'
+import CommunityList from '@/components/CommunityList.vue'
 
 
 defineProps({
-    communities:Object(),
+    communities:Object,
     posts:Object
 })
 </script>
