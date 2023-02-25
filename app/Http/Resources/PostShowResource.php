abf2313c75c9909c8b6e66013af86510573f81df
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2023-02-25 13:57:23 +0100

    adding moderators and is_admin

--- app/Http/Resources/PostShowResource.php
+++ app/Http/Resources/PostShowResource.php
@@ -23,7 +23,7 @@ class PostShowResource extends JsonResource
             'username'=>$this->user->username, 
             'slug'=>$this->slug,
             'url'=>$this->url,
-            'owner'=>auth()->id() ==$this->user_id ? true:false,
+            // 'owner'=>auth()->id() ==$this->user_id ? true:false,
             'comments'=>CommentResource::collection($this->whenLoaded('comments'))
 
         ];
